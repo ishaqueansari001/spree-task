0e7e9ebbef1a6b9a2d7519ca5db27a91e1d19f36
skmunaf1 <ishaqueansari001@gmail.com>
2024-01-04 16:09:21 +0530

    updated changes

---
+++ src/main/java/com/speer/ishaque/task/SpeerApplication.java
@@ -0,0 +1,14 @@
+package com.speer.ishaque.task;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeerApplication.class, args);
+    }
+
+}
